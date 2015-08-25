ec497582349eb10b276cee93f7b383b8ee5b7c54
karnoq <smith.sheldon@gmail.com>
2015-08-25 11:03:01 -0600

    Add while loop for multiple breaks

--- lesson_1/break_time.py
+++ lesson_1/break_time.py
@@ -3,5 +3,11 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=TRJYCW_dCN4")
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while break_count < total_breaks:
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=TRJYCW_dCN4")
+	break_count += 1
