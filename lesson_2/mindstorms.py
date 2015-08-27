7af31012dea5f8fa6be38a4e16982a7dea932d18
karnoq <smith.sheldon@gmail.com>
2015-08-27 15:18:10 -0600

    Cleaned up code

--- lesson_2/mindstorms.py
+++ lesson_2/mindstorms.py
@@ -1,29 +1,29 @@
 import turtle
 
 def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("red")
-    
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
-    
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
 
+    for i in range(4):
+        brad.forward(100)
+        brad.right(90)
+    
+
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
 
     angie.circle(100)
 
-    window.exitonclick()
+
+
+window = turtle.Screen()
+window.bgcolor("red")
 
 draw_square()
+draw_circle()
+
+window.exitonclick()
