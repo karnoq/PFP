8c13abfa03c09cbe7f392adc8e6e5e7e3d67a293
karnoq <smith.sheldon@gmail.com>
2015-08-27 16:06:31 -0600

    Draw a pretty shape

--- lesson_2/mindstorms.py
+++ lesson_2/mindstorms.py
@@ -6,9 +6,11 @@ def draw_square():
     brad.color("yellow")
     brad.speed(2)
 
-    for i in range(4):
-        brad.forward(100)
-        brad.right(90)
+    for i in range(1,37):
+        for k in range(4):
+            brad.forward(100)
+            brad.right(90)
+        brad.right(10)
     
 
 def draw_circle():
@@ -16,22 +18,24 @@ def draw_circle():
     angie.shape("arrow")
     angie.color("blue")
 
-    angie.circle(100)
+    for i in range(1, 37):
+        angie.circle(50)
+        angie.right(10)
 
-def draw_triangle():
-    jennifer = turtle.Turtle()
-    jennifer.shape("classic")
-    jennifer.color("black")
-
-    for i in range(3):
-        jennifer.forward(100)
-        jennifer.left(120)
+##def draw_triangle():
+##    jennifer = turtle.Turtle()
+##    jennifer.shape("classic")
+##    jennifer.color("black")
+##
+##    for i in range(3):
+##        jennifer.forward(100)
+##        jennifer.left(120)
 
 window = turtle.Screen()
 window.bgcolor("red")
 
 draw_square()
 draw_circle()
-draw_triangle()
+#draw_triangle()
 
 window.exitonclick()
