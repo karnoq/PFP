e2929cce7ddc1a63c611fac34c99d617e88d0b9d
karnoq <smith.sheldon@gmail.com>
2015-09-01 11:12:47 -0600

    Added function check_profanity

--- lesson_2/check_profanity.py
+++ lesson_2/check_profanity.py
@@ -4,4 +4,10 @@ def read_text():
     print contents_of_file
     quotes.close()
 
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    print output
+    connection.close()
+
 read_text()
