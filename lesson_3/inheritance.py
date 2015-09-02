e79d1bd137e42f9b0dee979033b4ac7554cd01b9
karnoq <smith.sheldon@gmail.com>
2015-09-02 10:34:57 -0600

    Add class Parent

--- lesson_3/inheritance.py
+++ lesson_3/inheritance.py
@@ -3,3 +3,6 @@ class Parent():
         print("Parent Constructor Called")
         self.last_name = last_name
         self.eye_color = eye_color
+
+billy_cyrus = Parent("Cyrus", "blue")
+print(billy_cyrus.last_name)
